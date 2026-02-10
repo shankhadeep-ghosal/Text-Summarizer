7dc767b3192855e75ccef00e9ba1a32f910821db
Shankhadeep Ghosal <ghosal.shankhadeep@gmail.com>
2026-02-10 23:52:52 +0530

    Upload Everything

--- textsummarizer/template.py
+++ textsummarizer/template.py
@@ -4,12 +4,13 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "text-summarizer"
+
+project_name = "textSummarizer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -20,26 +21,31 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
-    # "schema.yaml",
-    "main.py",
     "app.py",
+    "main.py",
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "reseaerch/trials.ipynb"
+    "research/trials.ipynb",
+
 ]
 
-for fiepath in list_of_files:
-    filepath = Path(fiepath)
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as fp:
+        with open(filepath,'w') as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
     else:
-        logging.info(f"{filename} already exists and is not empty. Skipping creation.")
+        logging.info(f"{filename} is already exists")
